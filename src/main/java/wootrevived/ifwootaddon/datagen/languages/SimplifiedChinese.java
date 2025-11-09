35096ffae4a3dfd63994d621cb9ab4aebeb87554
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-11-09 16:03:31 +0100

    Migrate to datagen

---
+++ src/main/java/wootrevived/ifwootaddon/datagen/languages/SimplifiedChinese.java
@@ -0,0 +1,21 @@
+package wootrevived.ifwootaddon.datagen.languages;
+
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.LanguageProvider;
+import wootrevived.ifwootaddon.IFWootAddon;
+import wootrevived.ifwootaddon.upgrades.LaserDrill;
+import wootrevived.ifwootaddon.upgrades.MobCrusher;
+import wootrevived.ifwootaddon.upgrades.MobSlaughterFactory;
+
+public class SimplifiedChinese extends LanguageProvider {
+    public SimplifiedChinese(PackOutput output){
+        super(output, IFWootAddon.MOD_ID, "zh_cn");
+    }
+
+    @Override
+    protected void addTranslations() {
+        add(MobCrusher.MOB_CRUSHER_ITEM.get(), "生物粉碎机升级");
+        add(MobSlaughterFactory.MOB_SLAUGHTER_FACTORY_ITEM.get(), "生物屠宰厂升级");
+        add(LaserDrill.LASER_DRILL_ITEM.get(), "镭射钻升级");
+    }
+}
